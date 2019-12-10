57e9cf1a9dd29048afe5e21c15ddb6c812d7a8cf
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-12-10 00:10:02 +0000

    fixing up the task 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,12 +12,13 @@
 
 int check_cycle(listint_t *list)
 {
-listint_t *tmp = list->next;
+listint_t *tmp;
 
 if (list == NULL)
 	{
 	return (0);
 	}
+tmp = list;
 while (tmp->next != NULL)
 	{
 	if (tmp->next == list)

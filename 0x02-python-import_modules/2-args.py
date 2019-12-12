25e5d91de5c2867d788efc58e01bc4189e8e109c
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-12-12 20:58:36 +0000

    add the task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        print("1 argument:")
+        print("{}:".format(str(len(sys.argv) -1)), sys.argv[1])
+    elif len(sys.argv) == 1:
+        print("0 arguments.")        
+    else:
+        print(str(len(sys.argv) -1), "arguments:")
+        for x in range(1, len(sys.argv)):
+            print("{}:".format(x), sys.argv[x])
+

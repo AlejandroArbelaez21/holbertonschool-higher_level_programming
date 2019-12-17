aabed894212a2de109c47b350e98619173679dc2
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-12-17 23:51:55 +0000

    add the task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    list = []
+    for x in range(len(my_list)):
+        if ((my_list[x] % 2) == 0):
+            list.append(True)
+        else:
+            list.append(False)
+    return(list)

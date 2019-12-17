4cee1fe9c2d479264dd5c0e70774e7a79383752c
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-12-17 22:37:42 +0000

    add the task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == "":
-        sentence = "None"
-    if sentence == " ":
-        sentence = "None"
+    while (sentence):
+        if sentence == "":
+            sentence = "None"
+        if sentence == " ":
+            sentence = "None"
     return (len(sentence), sentence[0])

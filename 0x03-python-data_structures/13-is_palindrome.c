a99f84a4f18ca62418f807d3405bcd5ef3e922f7
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-12-18 22:46:17 +0000

    fixing up the task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,21 +10,28 @@
  */
 int is_palindrome(listint_t **head)
 {
-int y, x = 0;
-int arr[1025];
+int y, x = 0, n;
+int arr[2047];
 listint_t *tmp = *head;
 
+if (!head)
+	{
+	return (0);
+	}
 while (tmp)
 	{
 	arr[x] = tmp->n;
 	x++;
 	tmp = tmp->next;
 	}
-for (y = 0; y < x / 2; y++)
-	if (arr[y] != arr[x])
+n = x;
+for (y = 0; y < n / 2; y++)
 	{
-	return (1);
+	if (arr[y] != arr[x - 1])
+	{
+	return (0);
 	}
 x--;
-return (0);
+	}
+return (1);
 }

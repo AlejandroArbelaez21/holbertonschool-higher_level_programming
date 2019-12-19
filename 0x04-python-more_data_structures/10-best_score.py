cb8b4e8354dac0cd8405175576c5b674fa3d5616
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-12-19 21:11:19 +0000

    fixing up the task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    y = 0
+    str = ""
     if (a_dictionary):
-        return(max(a_dictionary.keys()))
+        for x in a_dictionary:
+            if (a_dictionary[x] > y):
+                y = a_dictionary[x]
+                str = x
+        return(str)
     else:
         return (None)

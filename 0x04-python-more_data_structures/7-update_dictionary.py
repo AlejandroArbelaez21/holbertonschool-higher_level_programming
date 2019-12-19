0b25e8c5cbbaaf5edb6ae93ccfc35fd81024f89f
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-12-19 19:24:35 +0000

    fixing up the task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    for x in a_dictionary:
-        if (key in a_dictionary):
-            a_dictionary[key] = value
-        else:
-            d1 = {key: value}
-            a_dictionary.update(d1)
-        return(a_dictionary)
+    if (key in a_dictionary):
+        a_dictionary[key] = value
+    return(a_dictionary)

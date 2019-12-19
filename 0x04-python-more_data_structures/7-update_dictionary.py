2b1c7ee5614a790334917ee450403e08bfa2e84c
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-12-19 19:19:28 +0000

    add the task 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,8 +3,7 @@ def update_dictionary(a_dictionary, key, value):
     for x in a_dictionary:
         if (key in a_dictionary):
             a_dictionary[key] = value
-            return(a_dictionary)
         else:
             d1 = {key: value}
             a_dictionary.update(d1)
-            return(a_dictionary)
+        return(a_dictionary)

951be2f7848d59e0807ea8b2498cde05a99290ac
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2019-12-19 20:57:52 +0000

    add the task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    list = []
     if (a_dictionary):
-        for x, y in a_dictionary.items():
-            return(x)
+        return(max(a_dictionary.keys()))
     else:
         return (None)

e570a2001bbc276e5a7da7e3c442add331cc44bf
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2019-12-20 04:19:56 +0000

    an advanced of the task

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if (roman_string):
-        my_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         sum = 0
-        for x, y in my_dict.items():
-            if my_dict[x] == roman_string:
-                sum = sum + my_dict[y]
+        for x, y in dic.items():
+            if dic[x] == roman_string:
+                sum = sum + dic[y]
         return (sum)
-    return(0)    
+    return(0)

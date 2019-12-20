bb46279bc951bb93bc29de270bf63de9eca3de23
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2019-12-20 01:38:33 +0000

    add the task lambda

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     list = matrix.copy()
-    for x in range(len(list)):
-        for y in range(len(list[x])):
-            return (list)
+    return [[x*x for x in row] for row in list]

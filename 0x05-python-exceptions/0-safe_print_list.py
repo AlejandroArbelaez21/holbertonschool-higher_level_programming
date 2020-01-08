bbe7679e40db8ddbe9eb9eca58834013a60db3b1
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-01-08 17:44:27 +0000

    fixing up the task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
+        cont = 0
         for y in range(x):
             print(my_list[y], end="")
+            cont = cont + 1
         print()
-        return (y + 1)
+        return (cont)
     except IndexError:
         print()
-        return (y)
+        return (cont)

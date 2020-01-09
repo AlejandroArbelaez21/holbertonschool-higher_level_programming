b74798c1e98662ffb72b8370b8e6c10650d26a58
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-01-09 00:28:06 +0000

    advanced

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,5 +7,7 @@ def safe_function(fct, *args):
         return (fct(args[0], args[1]))
     except ZeroDivisionError as error:
         sys.stderr.write("Exception: {}\n".format(error))
+        return (None)
     except IndexError as error:
         sys.stderr.write("Exception: {}\n".format(error))
+        return (None)

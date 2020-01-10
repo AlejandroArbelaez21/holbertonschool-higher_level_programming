952e776e93dade0194f46bdc60da39230f6630df
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2020-01-10 05:10:10 +0000

    add the super hard task number 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -33,7 +33,8 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for x in range(self.__size):
-                for y in range(self.__size):
+            for y in range(self.__size):
+                print(" " * self.__position[0], end="")
+                for x in range(self.__size):
                     print('#', end="")
                 print()

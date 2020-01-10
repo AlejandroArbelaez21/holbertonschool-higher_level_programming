7fa728a6b8f15d402cbd78d79f2e4ceb772a3b18
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2020-01-10 05:17:59 +0000

    add the super hard task number 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -25,9 +25,9 @@ class Square:
 
     @size.setter
     def position(self, value):
-        if value[0] < 0 and value[1] < 0:
+        if value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if type(value[0]) is not int and type(value[1]) is not int:
+        if type(value[0]) is not int or type(value[1]) is not int:
             raise TypeError("position must be a tuple of 2 positive integers")
             self.__position = value
 

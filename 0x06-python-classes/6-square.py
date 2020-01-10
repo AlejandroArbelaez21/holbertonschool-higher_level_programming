79dbe90696a70f043f537fb3f374a7eb6eafaf10
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2020-01-10 04:52:40 +0000

    add the super hard task number 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,6 +11,10 @@ class Square:
     def size(self):
         return (self.__size)
 
+    @property
+    def position(self):
+        return (self.__position)
+
     @size.setter
     def size(self, value):
         if type(value) is not int:
@@ -18,10 +22,6 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
-    @property
-    def position(self):
-        return (self.__position)
 
     @size.setter
     def position(self, value):

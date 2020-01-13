65dbff3eb0d22aaabce9059913bd80da85a087cb
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-01-13 17:37:07 -0500

    The cases of error of the project

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,3 +1,29 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
-
+    other = []
+    text = "matrix must be a matrix (list of lists) of integers/floats"
+    text2 = "Each row of the matrix must have the same size"
+    size = len(matrix[0])
+    if (matrix):
+        if type(div) is not int and type(div) is not float:
+            raise TypeError("div must be a number")
+        if div == 0:
+            raise ZeroDivisionError("division by zero")
+        if type(matrix) is not list:
+            raise TypeError(text)
+        for y in range(len(matrix)):
+            if type(matrix[y]) is not list:
+                raise TypeError(text)
+        for x in matrix:
+            if size != len(x):
+                raise TypeError(text2)
+        for row in matrix:
+            for elem in row:
+                if type(elem) is not int and type(elem) is not float:
+                    raise TypeError(text)
+        for row in matrix:
+            new = []
+            for elem in row:
+                new.append(round(elem / div, 2))
+            other.append(new)
+    return(other)
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 def text_indentation(text):
-    if (text):
+    if text:
         if type(text) is not str:
             raise TypeError("text must be a string")
         else:
-            for x in range(len(text)):
-                if text[x] == "." or text[x] == "?" or text[x] == ":":
-                    print(text[x])
+            cont = 0
+            sum = 0
+            for y in range(len(text)):
+                if text[y] == "." or text[y] == "?" or text[y] == ":":
+                    sum += 1
+            for x in range(len(text) - sum):
+                if text[cont] == "." or text[cont] == "?" or text[cont] == ":":
+                    print(text[cont])
                     print()
+                    cont += 1
+                    while text[cont] == " ":
+                        cont += 1
                 else:
-                    print(text[x], end="")
-            print()
+                    print(text[cont], end="")
+                    cont += 1

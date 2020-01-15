e64d34dcbe4c3e3500732afc9c330e6999c5ec3f
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2020-01-15 05:13:38 +0000

    delete the space

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -49,4 +49,4 @@ class Rectangle:
             return my_str.strip('\n')
 
     def __repr__(self):
-        return ("Rectangle ({}, {})".format(self.__width, self.__height))
+        return ("Rectangle({}, {})".format(self.__width, self.__height))

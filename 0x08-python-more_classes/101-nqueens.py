3e1d2f207814a9db2f41dbb8a43aba99f22f2b9c
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2020-01-15 05:06:14 +0000

    correction pep8

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -2,9 +2,9 @@
 from sys import argv
 if len(argv) != 2:
     print("Usage: nqueens N")
-    exit (1)
+    exit(1)
 elif not isinstance(argv[1], int):
     print("N must be a number")
-    exit (1)
+    exit(1)
 elif argv[1] < 4:
-    print("N must be at least 4")    
+    print("N must be at least 4")

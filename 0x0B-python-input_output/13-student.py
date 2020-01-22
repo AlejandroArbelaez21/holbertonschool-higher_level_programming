486e101914738dbc29a63d82862b5a2759a31514
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2020-01-22 05:53:44 +0000

    add the task 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+class Student:
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return (self.__dict__)
+        else:
+            my_dict = {}
+            for y in attrs:
+                if y in self.__dict__:
+                    my_dict[y] = self.__dict__[y]
+            return (my_dict)
+
+    def reload_from_json(self, json):
+        if type(json) is dict:
+            self.first_name = json["first_name"]
+            self.last_name = json["last_name"]
+            self.age = json["age"]

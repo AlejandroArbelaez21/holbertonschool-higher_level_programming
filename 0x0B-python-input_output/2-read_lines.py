c4cdccbe5878643c58febd2182a7e388d554034d
AlejandroArbelaez21 <alejandroarbelaez21@gmail.com>
2020-01-22 04:54:19 +0000

    correction

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -2,8 +2,9 @@
 def read_lines(filename="", nb_lines=0):
     with open(filename, mode="r", encoding="UTF-8") as file:
         if nb_lines <= 0 or nb_lines >= len(file.readlines()):
-            print(file.read(), end="")
+            with open(filename, mode="r", encoding="UTF-8") as file:
+                print(file.read(), end="")
         else:
-            len_lines = file.readlines()
-            for line in range(nb_lines):
-                print(len_lines[line], end="")
+            with open(filename, mode="r", encoding="UTF-8") as file:
+                for line in range(nb_lines):
+                    print(file.readline(), end="")

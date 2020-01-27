f67c26e4a06e515503c25f95fb74d9b5543ca27f
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-01-27 16:45:28 -0500

    correction

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -12,8 +12,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ define the __init__
-        """
+        """ define the __init__"""
         if id is not None:
             self.id = id
         else:
@@ -21,11 +20,17 @@ class Base:
             self.id = Base.__nb_objects
 
     @staticmethod
-    """
-    Static method for json string
-    """
     def to_json_string(list_dictionaries):
+        """ define the staticmethod"""
         if list_dictionaries is None:
             return "[]"
         else:
             return dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ define the classmethod"""
+        if list_objs is None:
+            str_json = "[]"
+        else:
+            return cls.to_json_string(list_objs)

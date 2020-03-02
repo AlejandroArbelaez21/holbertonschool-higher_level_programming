6951419e6055547ce3bec65bb94f830905581d66
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-03-02 18:51:34 -0500

    advanced

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,3 @@
+-- displays the number of records with id = 89 in the table first_table
+-- count commands that count the once that are the words in specific in table of a databases
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;

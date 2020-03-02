de7e266809c02d2d02f4b7d17bab18c041c701be
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-03-02 18:37:14 -0500

    add the tasks

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- displays the number of records with id = 89 in the table first_table
 -- count commands that count the once that are the words in specific in table of a databases
-SELECT score, COUNT(*) AS number FROM second_table GROUP BY score;
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY score DESC;

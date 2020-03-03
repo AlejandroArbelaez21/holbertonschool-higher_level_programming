fe0ade7d4a26d7dd7d612061b57a5cec68ee6587
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-03-03 16:18:20 -0500

    add the tasks

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,3 @@
 -- create and grants create a new user and set a privileges
-SELECT cities.id, cities.name, states.name FROM states JOIN cities ORDER BY cities.id ASC;
+-- create and grants create a new user and set a privileges
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ORDER BY cities.id ASC;

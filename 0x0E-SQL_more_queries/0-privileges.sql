d5ae58c0f13486728e758efcb4f01745a85ae378
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-03-03 12:04:27 -0500

    add the tasks

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- display the privileges for the users
+-- show grants show the privileges
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- creates the MySQL server user
+-- create and grants create a new user and set a privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';

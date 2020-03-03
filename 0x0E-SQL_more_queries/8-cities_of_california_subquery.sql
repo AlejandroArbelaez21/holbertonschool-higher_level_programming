e4901020835face12000a5e16b0b8cbb0b82abbe
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-03-03 15:35:50 -0500

    add the tasks

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- creates the MySQL server user
 -- create and grants create a new user and set a privileges
-SELECT * FROM cities WHERE state_id = 1 ORDER BY id;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;

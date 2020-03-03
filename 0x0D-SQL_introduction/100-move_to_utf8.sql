a5d0d561dfeef5c63dfbd5fc5bbbf5c9ec956f3c
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-03-03 01:46:15 +0000

    advanced

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,6 @@
 -- displays the number of records with id = 89 in the table first_table
 -- count commands that count the once that are the words in specific in table of a databases
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

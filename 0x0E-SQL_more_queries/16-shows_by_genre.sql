018cc33eb435f39b5433df5820abf92896bdc9a4
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-03-04 02:18:09 +0000

    add the task

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,5 +2,5 @@
 -- create and grants create a new user and set a privileges
 SELECT tv_shows.title, tv_genres.name FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_genres._id = tv_show.genre_id
+LEFT JOIN tv_genres ON tv_genres._id = tv_show_genres.genre_id
 ORDER BY tv_shows.title, tv_genres.name ASC;

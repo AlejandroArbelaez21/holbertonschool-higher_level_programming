73a235db3356850366b5cbaa76dcdd8005225f9b
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-03-05 04:30:22 +0000

    advanced

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,9 @@
+-- create and grants create a new user and set a privileges
+-- create and grants create a new user and set a privileges
+SELECT tv_genres.name,
+SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id 
+JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.name
+ORDER BY rating DESC;

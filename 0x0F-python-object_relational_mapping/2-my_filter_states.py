fcfd6d0d8d6a46951f4898c5909e7e63444ede70
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-03-29 01:12:17 -0500

    learning about of MySQLdb with python

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     c = db.cursor()
-    c.execute("""SELECT * FROM states WHERE name = %s ORDER BY id""",
-              (argv[4],))
+    c.execute("""SELECT * FROM states WHERE name='{}'
+              ORDER BY id""".format(argv[4]))
 
     """ loop for print the states """
     for state in c.fetchall():

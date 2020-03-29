681e71759460e414af5fbaf163a6f21876785700
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-03-29 01:43:30 -0500

    learning about of MySQLdb with python

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,12 +11,12 @@ from sys import argv
 if __name__ == "__main__":
     """ Here realice a query and make a cursor """
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], argument=argv[4])
+                         passwd=argv[2], db=argv[3])
     c = db.cursor()
     c.execute("""SELECT * FROM states WHERE name='{}'
-              ORDER BY id""".format(argument))
+              ORDER BY id""".format(argv[4]))
 
     """ loop for print the states """
     for state in c.fetchall():
-        if state[1] == argument:
+        if state[1] == argv[4]:
             print(state)

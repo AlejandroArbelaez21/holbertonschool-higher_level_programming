6a4b40a85fe29806a72be0689ebd5afebeb42634
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-03-28 23:36:35 -0500

    learning about of MySQLdb with python

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+import the module MySQLdb and argv from sys
+"""
+
+
+import MySQLdb
+from sys import argv
+
+""" This prevents that my code should not be executed when imported """
+if __name__ == "__main__":
+    """ Here realice a query and make a cursor """
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    c = db.cursor()
+    c.execute("""SELECT * FROM states ORDER BY id""")
+
+    """ loop for print the states """
+    for state in c.fetchall():
+        if state[1] == argv[4]:
+            print(state)

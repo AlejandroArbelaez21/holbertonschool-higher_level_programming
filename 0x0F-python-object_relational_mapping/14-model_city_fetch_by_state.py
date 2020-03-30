a4759ad842fb0292eba5e1934594dcd0720a7c81
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-03-30 00:19:37 -0500

    learning about of MySQLdb with python

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -9,12 +9,13 @@ from sqlalchemy.orm import sessionmaker
 from sys import argv
 from model_city import City
 
+
 if __name__ == "__main__":
     engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format
                            (argv[1], argv[2], argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
     for instance, city in session.query(State, City)\
-                                 .filter(State.id == City.instance.id)\
+                                 .filter(State.id == City.state_id)\
                                  .order_by(City.id):
         print("{}: ({}) {}".format(instance.name, city.id, city.name))
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,13 +5,14 @@ import the sqlalchemy and declarative_base
 
 
 from model_state import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class City(Base):
     """ Class City inheritance for Base"""
-    __tablename__ = 'states'
+    __tablename__ = "cities"
 
     id = Column(Integer, nullable=False, unique=True, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, nullable=False, primary_key=True, ForeignKey("states.id"))
+    state_id = Column(Integer, nullable=False,
+                      ForeignKey("states.id"))

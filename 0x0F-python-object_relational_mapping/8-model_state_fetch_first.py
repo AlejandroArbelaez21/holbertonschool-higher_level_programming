e0c19af3f8e7729436bc4f8e70a45cc09e6bdcb0
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-03-29 19:10:57 -0500

    learning about of MySQLdb with python

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+import all mod
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format
+                           (argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        if instance is None:
+            print("Nothing")
+        if instance.id == 1:
+            print("{}: {}".format(instance.id, instance.name))

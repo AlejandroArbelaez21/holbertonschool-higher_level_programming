e17de5afbfcd595f8e715631af2814a54c375c5e
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-03-29 23:19:55 -0500

    learning about of MySQLdb with python

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+import all mod
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    engine = create_engine('mysql://{}:{}@localhost:3306/{}'.format
+                           (argv[1], argv[2], argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    instance = session.query(State).filter_by(id=2).first()
+    instance.name = "New Mwxico"
+    session.commit()

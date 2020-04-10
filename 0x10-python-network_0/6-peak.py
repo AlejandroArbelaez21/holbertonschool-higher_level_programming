e2f44dad9844547e7b3e47df10a616cff3b44141
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-04-10 17:18:40 -0500

    add the peak funtion

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""
+This module contains find_peak funtion
+"""
+
+
 def find_peak(list_of_integers):
+    """ finds a peak in a list of unsorted integers """
     if list_of_integers == []:
-        return "None"
+        return None
     else:
         return max(list_of_integers)

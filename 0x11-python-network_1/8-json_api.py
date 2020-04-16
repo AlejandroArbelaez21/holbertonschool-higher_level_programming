338481c5a61e8adde7625040ff2ca469cdd77af8
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-04-16 18:22:59 -0500

    learning about of request in python

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,6 +10,7 @@ from sys import argv
 """ This prevent that my code not be executed when imported """
 if __name__ == "__main__":
     """The funtion"""
+    a = argv[1][0]
     if len(argv) == 1:
         r = requests.post('http://0.0.0.0:5000/search_user', data={'q': ''})
         print("No result")
@@ -17,7 +18,10 @@ if __name__ == "__main__":
         r = requests.post('http://0.0.0.0:5000/search_user', data={'q': ''})
         print("No result")
     else:
-        r = requests.post('http://0.0.0.0:5000/search_user', data={'q': argv[1]})
-        id = r.text[6:10]
-        letter = r.text[19:-3]
-        print("[{}] {}".format(id, letter))
+        r = requests.post('http://0.0.0.0:5000/search_user', data={'q': a})
+        if r.text[0] != '{' and r.text[-1] != '}':
+            print("Not a valid JSON")
+        else:
+            id = r.text[6:10]
+            letter = r.text[19:-3]
+            print("[{}] {}".format(id, letter))

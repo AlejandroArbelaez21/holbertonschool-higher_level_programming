f171779da8171e80676568e5940183b50f8c760c
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-04-16 12:43:36 -0500

    learning about of request in python

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,13 +4,11 @@ import urllib
 """
 
 
-import urllib.request
+import requests
 """ This prevent that my code not be executed when imported """
 if __name__ == "__main__":
     """The funtion"""
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        the_page = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(str(the_page))))
-        print("\t- content: {}".format(the_page.decode('utf8')))
+    req = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(str(req))))
+    print("\t- content: {}".format(req.text))

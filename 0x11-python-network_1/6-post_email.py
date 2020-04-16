472573520208756f2a03127a3610e429cc2637b6
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-04-16 14:54:49 -0500

    learning about of request in python

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+import requests
+"""
+
+
+import requests
+from sys import argv
+
+""" This prevent that my code not be executed when imported """
+if __name__ == "__main__":
+    """The funtion"""
+    r = requests.post(argv[1], data = {'email': argv[2]})
+    print(r.text)

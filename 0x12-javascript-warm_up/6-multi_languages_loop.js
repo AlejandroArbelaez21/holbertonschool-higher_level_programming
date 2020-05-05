f3dda9e176edaff1cb1348349a9fb96dda80c389
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-05-05 16:48:43 -0500

    loop js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-var languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let index = 0; index < languages.length; index++) {
   console.log(languages[index]);
 }

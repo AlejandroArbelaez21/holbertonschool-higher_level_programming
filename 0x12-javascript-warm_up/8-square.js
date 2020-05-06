5ef67bc6a289a11b0d630cbf5274a86f4115d0e7
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-05-05 23:23:26 -0500

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-const number = parseInt(process.argv[2]);
+const number = process.argv[2];
 if (number === undefined) {
+  console.log('Missing size');
+} else if (isNaN(number)) {
   console.log('Missing size');
 }
 for (let index = 0; index < number; index++) {
-  console.log('X'.repeat(number));
+  console.log('X'.repeat(parseInt(number)));
 }

cba436ac259555020e116769c0c64f6f2fd54e83
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-05-05 22:34:14 -0500

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const number = parseInt(process.argv[2]);
+const number = process.argv[2];
 if (isNaN(number)) {
   console.log(0);
 } else if (process.argv.length < 4) {
@@ -7,8 +7,10 @@ if (isNaN(number)) {
 } else {
   const list = [];
   for (let index = 2; index < process.argv.length; index++) {
-    list.push(process.argv[index]);
+    list.push(parseInt(process.argv[index]));
   }
-  list.sort();
+  list.sort(function (a, b) {
+    return a - b;
+  });
   console.log(list[list.length - 2]);
 }

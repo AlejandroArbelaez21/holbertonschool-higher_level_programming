d050c236ad3cc1cabb0cb7ffc7bbd71712211be3
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-05-05 23:40:23 -0500

    advanced

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.callMeMoby = callMeMoby;
 function callMeMoby (x, theFunction) {
-    for (let index = 0; index < x; index++) {
-      theFunction();
-    }
+  for (let index = 0; index < x; index++) {
+    theFunction();
   }
+}

0504d6cbdffe4de409bcb43551826bb01aa274a4
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-05-05 22:55:17 -0500

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const number = process.argv[2];
+if (number === undefined) {
+  console.log('Missing size');
+}
+for (let index = 0; index < number; index++) {
+    console.log('X'.repeat(process.argv[2]));
+}

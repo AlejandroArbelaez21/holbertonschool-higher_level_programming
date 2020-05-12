e344a734e60de095fe427e65a2db7a40c8f543eb
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-05-11 23:54:00 -0500

    advanced

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const Map = require('./100-data');
-const newList = [];
-for (let index = 0; index < Map.list.length; index++) {
-  const map1 = Map.list.map((x, index) => x * index);
-  newList[index] = map1[index];
-}
+
+const map1 = Map.list.map((x, index) => x * index);
+
 console.log(Map.list);
-console.log(newList);
+console.log(map1);

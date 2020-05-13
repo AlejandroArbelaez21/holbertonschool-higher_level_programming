f656686d21a6f34bf38ed98f580c6f6e0576ffbc
AlejandroArbelaez21 <alejandro.arbelaez21@gmail.com>
2020-05-12 22:27:53 -0500

    Web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const requests = require('request');
 
-requests(`http://swapi.co/api/films/${process.argv[2]}`, function writestatus (err, body) {
+requests(`http://swapi.co/api/films/${process.argv[2]}`, function writestatus (err, response, body) {
   if (err) {
     console.log(err);
   } else {
-    const mTitle = JSON.parse(body).title;
-    console.log(mTitle);
+    console.log(JSON.parse(body).title);
   }
 });
